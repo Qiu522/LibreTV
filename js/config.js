b82e12eefdf236a994bf310aaa41aa2210894b4e
Qiu522 <40030425+Qiu522@users.noreply.github.com>
2025-08-04 16:36:52 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,6 +22,81 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
+    "mtzy": {
+      "api": "https://caiji.maotaizy.cc/api.php/provide/vod",
+      "name": "茅台资源",
+      "detail": "https://caiji.maotaizy.cc"
+    },
+    "bdzy": {
+      "api": "https://api.apibdzy.com/api.php/provide/vod",
+      "name": "百度资源",
+      "detail": "https://api.apibdzy.com"
+    },
+    "tyyszy": {
+      "api": "https://tyyszyapi.com/api.php/provide/vod",
+      "name": "天涯影视资源",
+      "detail": "https://tyyszyapi.com"
+    },
+    "aqyzy": {
+      "api": "https://iqiyizyapi.com/api.php/provide/vod",
+      "name": "爱奇艺资源",
+      "detail": "https://iqiyizyapi.com"
+    },
+    "wxzy": {
+      "api": "https://api.wuxianzy.net/api.php/provide/vod",
+      "name": "无线资源",
+      "detail": "https://api.wuxianzy.net"
+    },
+    "fcpk": {
+      "api": "https://api.fczy888.me/api.php/provide/vod",
+      "name": "蜂巢片库",
+      "detail": "https://api.fczy888.me"
+    },
+    "wjzy": {
+      "api": "https://api.wujinapi.me/api.php/provide/vod",
+      "name": "无尽资源",
+      "detail": "https://api.wujinapi.me"
+    },
+    "yhzy": {
+      "api": "https://m3u8.apiyhzy.com/api.php/provide/vod",
+      "name": "樱花资源",
+      "detail": "https://m3u8.apiyhzy.com"
+    },
+    "qlzy": {
+      "api": "https://www.qilinzyz.com/api.php/provide/vod",
+      "name": "麒麟资源",
+      "detail": "https://www.qilinzyz.com"
+    },
+    "bfzy": {
+      "api": "https://bfzyapi.com/api.php/provide/vod",
+      "name": "暴风资源",
+      "detail": "https://bfzyapi.com"
+    },
+    "jszy": {
+      "api": "https://jszyapi.com/api.php/provide/vod",
+      "name": "极速资源",
+      "detail": "https://jszyapi.com"
+    },
+    "mddmzy": {
+      "api": "https://www.mdzyapi.com/api.php/provide/vod",
+      "name": "魔都动漫资源",
+      "detail": "https://www.mdzyapi.com"
+    },
+    "hmezy": {
+      "api": "https://json.heimuer.xyz/api.php/provide/vod",
+      "name": "黑木耳资源",
+      "detail": "https://json.heimuer.xyz"
+    },
+    "360zy": {
+      "api": "https://360zy.com/api.php/provide/vod",
+      "name": "360资源",
+      "detail": "https://360zy.com"
+    },
+    "gszy": {
+      "api": "https://api.guangsuapi.com/api.php/provide/vod",
+      "name": "光速资源",
+      "detail": "https://api.guangsuapi.com"
+    },
     dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
@@ -47,11 +122,6 @@ const API_SITES = {
         api: 'http://ffzy5.tv/api.php/provide/vod',
         name: '非凡影视',
         detail: 'http://ffzy5.tv', 
-    },
-    heimuer: {
-        api: 'https://json.heimuer.xyz/api.php/provide/vod',
-        name: '黑木耳',
-        detail: 'https://heimuer.tv', 
     },
     zy360: {
         api: 'https://360zy.com/api.php/provide/vod',
@@ -127,16 +197,16 @@ const API_SITES = {
     // 这些影片和背后的产业已经使数百万男女“下海”，出卖自己的身体，甚至以此为生计。
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
-    // ckzy: {
-    //     api: 'https://www.ckzy1.com',
-    //     name: 'CK资源',
-    //     adult: true
-    // },
-    // jkun: {
-    //     api: 'https://jkunzyapi.com',
-    //     name: 'jkun资源',
-    //     adult: true
-    // },
+     ckzy: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源',
+        adult: true
+     },
+    jkun: {
+         api: 'https://jkunzyapi.com',
+         name: 'jkun资源',
+         adult: true
+     },
     // bwzy: {
     //     api: 'https://api.bwzym3u8.com',
     //     name: '百万资源',
@@ -147,11 +217,11 @@ const API_SITES = {
     //     name: 'souav资源',
     //     adult: true
     // },
-    // r155: {
-    //     api: 'https://155api.com',
-    //     name: '155资源',
-    //     adult: true
-    // },
+     r155: {
+         api: 'https://155api.com',
+        name: '155资源',
+        adult: true
+     },
     // lsb: {
     //     api: 'https://apilsbzy1.com',
     //     name: 'lsb资源',
